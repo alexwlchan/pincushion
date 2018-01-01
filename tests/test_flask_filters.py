3199c386027d26d62e0cced2d56dd686ba3b52d0
Alex Chan <alex@alexwlchan.net>
2018-01-01 10:04:31 +0000

    Add test cases for the new tag sort

--- tests/test_flask_filters.py
+++ tests/test_flask_filters.py
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8
 
+from hypothesis import assume, given
+from hypothesis.strategies import lists, text
 import pytest
 
 from pincushion.flask import filters
@@ -17,3 +19,25 @@ def test_title_markdown(md, expected_html):
 def test_multiline_title_markdown_is_error():
     with pytest.raises(ValueError):
         filters.title_markdown('This is a\nmulti-line string\nof Markdown')
+
+
+@pytest.mark.parametrize('tags, expected_sorted_tags', [
+    (['rust'], ['rust']),
+    (['politics', 'brexit'], ['brexit', 'politics']),
+
+    # Doing the word count tags properly is a key feature of the new sorting
+    # system
+    (['fic', 'wc:<1k'], ['fic', 'wc:<1k']),
+    (['wc:1k-5k', 'gen', 'wc:<1k'], ['gen', 'wc:<1k', 'wc:1k-5k']),
+    (['wc:10k-25k', 'wc:<1k', 'wc:1k-5k', 'wc:5k-10k'],
+     ['wc:<1k', 'wc:1k-5k', 'wc:5k-10k', 'wc:10k-25k']),
+])
+def test_custom_tag_sort(tags, expected_sorted_tags):
+    assert filters.custom_tag_sort(tags) == expected_sorted_tags
+
+
+@given(lists(text()))
+def test_custom_tag_sort_ignores_non_wc_tags(tags):
+    assume(not any(t.startswith('wc:') for t in tags))
+    result = filters.custom_tag_sort(tags)
+    assert result == sorted(tags)
