3cc9d13fa588c420c71f134182e4d5e0497788f6
Alex Chan <alex@alexwlchan.net>
2018-01-20 23:31:09 +0000

    Improve indexing performance

--- src/pincushion/search.py
+++ src/pincushion/search.py
@@ -2,6 +2,8 @@
 
 import os
 
+import maya
+
 from whoosh.fields import SchemaClass, ID, KEYWORD, DATETIME
 from whoosh.index import create_in
 
@@ -18,9 +20,24 @@ def create_index(schema):
 
 
 def index_documents(index, documents):
-    writer = index.writer()
+    import time
+    t = time.time()
+    writer = index.writer(limitmb=256)
+
+
+    last_modified = maya.MayaDT(index.last_modified()).datetime()
+    is_empty = index.is_empty()
+    print(last_modified)
+
+    doc_ids = []
+
     for doc in documents:
-        writer.update_document(**doc)
+        doc_ids.append(doc['id'])
+
+        if is_empty or (doc['time'] >= last_modified):
+            writer.update_document(**doc)
     writer.commit()
+    print(f'Reindexed in {time.time() - t}')
 
     # And delete old bookmarks!!
+    # mergetype=writing.CLEAR?
