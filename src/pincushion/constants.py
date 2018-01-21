5cbb877d9d5a1208f1f5834082aa8e5a089b836a
Alex Chan <alex@alexwlchan.net>
2018-01-21 14:30:47 +0000

    Drop the elasticsearchpy dependency

--- src/pincushion/constants.py
+++ src/pincushion/constants.py
@@ -1,16 +1,4 @@
 # -*- encoding: utf-8
 
-import os
-
-from elasticsearch import Elasticsearch
-
-
 S3_BUCKET = 'alexwlchan-pincushion'
 S3_BOOKMARKS_KEY = 'bookmarks.json'
-
-INDEX_NAME = 'bookmarks'
-DOC_TYPE = 'bookmarks'
-
-ES_HOST = (
-    os.environ.get('ELASTICSEARCH_HOST', 'http://localhost:9200/').rstrip('/'))
-ES_CLIENT = Elasticsearch(hosts=[ES_HOST])
